d96bb8ccdd89d3be3042c04af23eed0cd20a012f
LETICIA SOUZA <leleetina2004@gmail.com>
2026-07-15 17:57:04 -0300

    meu primeiro trabalho
    Atividade 01 – Variáveis e Tipos de Dados em Python
    Data de conclusão 4 de julho de 2026 às 13:00
    •
    Fecha 4 de julho de 2026 às 13:00
    Instruções
    Objetivo
    Praticar a criação de variáveis utilizando os principais tipos de dados da linguagem Python.
    
    Instruções
    Você deverá criar um programa em Python contendo as variáveis descritas na tabela abaixo.
    
    Importante:
    
    Não copie o código de outro colega.
    Digite todo o código manualmente.
    Utilize exatamente os nomes das variáveis informados.
    Ao final, utilize print() para exibir o conteúdo de todas as variáveis.
    Variáveis que deverão ser criadas
    Nome da variável	Tipo de dado	Valor
    marca	String	Toyota
    modelo	String	Corolla
    ano	Inteiro (int)	2023
    preco	Decimal (float)	145000.00
    acessorios	Lista (list)	Ar-condicionado, Multimídia e Airbag
    dimensoes	Tupla (tuple)	4.63, 1.78 e 1.45
    carro	Dicionário (dict)	Deve conter as informações de marca, modelo, ano e preço utilizando as variáveis já criadas.
    
    Exiba os resultados
    Após criar todas as variáveis, exiba cada uma delas utilizando o comando print().
    
    A saída deverá apresentar, nesta ordem:
    
    Marca
    Modelo
    Ano
    Preço
    Lista de acessórios
    Tupla de dimensões
    Dicionário do carro

---
+++ atividade01.py
@@ -0,0 +1,8 @@
+Marca = 'Toyota'
+Modelo = 'Corolla'
+Ano = 2023
+Preço = 145000.00
+Acessorios = ['Ar-condicionado','Multimídia e Airbag']
+Dimensoes = ('4.63','1.78','1.45')
+carro = {'marca1': Marca,'modelo':Modelo,'ano':Ano, 'preco':Preço,'acessorio': Acessorios,'dimensoes':Dimensoes}
+print (carro)
