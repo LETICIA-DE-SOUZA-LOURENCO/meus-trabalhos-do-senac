d33485c03ac862806d5bd2fe57dec7f6fb042de9
LETICIA SOUZA <leleetina2004@gmail.com>
2026-07-17 20:07:32 -0300

    Add files via upload

---
+++ atividade02.py
@@ -0,0 +1,30 @@
+titulo = 'Interestelar'
+diretor = 'Christopher Nolan'
+ano = 2014
+duracao = 169.0
+generos = [
+    'Ficção Científica,'
+    'Drama,'
+    'Aventura'
+]
+elenco_principal = (
+    'Matthew McConaughey',
+    'Anne Hathaway',
+    'Jessica Chastain.'
+)
+filme = {
+    'titulo': titulo,
+    'diretor': diretor,
+    'ano': ano,
+    'duracao': duracao,
+    'generos': generos,
+    'elenco_principal': elenco_principal
+}
+print(filme) 
+print(type(titulo))
+print(type(diretor))
+print(type(ano))
+print(type(duracao))
+print(type(generos))
+print(type(elenco_principal))
+print(type(filme))
