1658ff5e16aa621fb9c4ab065a38a71c05410aad
LETICIA SOUZA <leleetina2004@gmail.com>
2026-07-19 20:31:30 -0300

    SIMULAR DE NOTA ESCOLAR (SIMPLES)
    Atividade: Simulador de Nota Escolar (Versão Simples)
    •
    Instruções
    Objetivo: Praticar a entrada de dados, uso de variáveis e operações matemáticas.
    
    Passos da Atividade
    Peça ao usuário para inserir três notas (pode informar que as notas variam de 0 a 10).
    Calcule a média das notas.
    Exiba a média final na tela.

---
+++ atividade03.py
@@ -0,0 +1,5 @@
+n1 = float (input("Digite a primeira nota de 0 a 10: "))
+n2 = float (input("Digite a segunda nota de 0 a 10: "))
+n3 = float (input("Digite a terceira nota de 0 a 10: "))
+media = (n1 + n2 + n3) // 3
+print(f"A média das notas é: {media}")
